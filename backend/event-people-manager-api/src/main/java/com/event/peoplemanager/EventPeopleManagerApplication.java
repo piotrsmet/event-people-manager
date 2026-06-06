c1d7e0cf8e5f997b0636a5a39647d5da3289992f
smecik48 <piotrsmet@interia.pl>
2026-06-06 19:54:59 +0200

    chore(backend): init spring boot project with jpa, postgres and flyway

---
+++ backend/event-people-manager-api/src/main/java/com/event/peoplemanager/EventPeopleManagerApplication.java
@@ -0,0 +1,13 @@
+package com.event.peoplemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventPeopleManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventPeopleManagerApplication.class, args);
+	}
+
+}
